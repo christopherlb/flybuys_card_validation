b9d2d214ec644c35cf7f4d61802e84d962fa96a6
Chris Bruniges <chris.bruniges@ikegps.com>
2021-06-27 11:31:10 +1200

    Inital logic, failing test

--- spec/validator.spec.rb
+++ spec/validator.spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
+require 'validator'
 
-RSpec.describe  do
-  it 'passes' do
-    expect(true).to be_truthy
+RSpec.describe Validator do
+  let(:short_card) { '1234567890' }
+
+  it 'stores the card number' do
+    expect(Validator.new(short_card).card_number).to eq(short_card)
   end
 
-  it 'fails' do
-    expect(true).to be_falsey
+  context 'with a short card' do
+    subject { Validator.new(short_card) }
+    it 'shows short card nos as invalid' do
+      expect(subject.valid).to be_falsey
+    end
   end
 end
--- validator.rb
+++ validator.rb
@@ -1,1 +1,10 @@
+class Validator
+  attr_reader :card_number
+
+  def initialize(card_number)
+    @card_number = card_number
+  end
+end
+
 puts "hello world"
+puts Validator.new('123').card_number
